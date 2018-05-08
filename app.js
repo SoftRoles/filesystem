d8b730944ef67ac98f54da4588f7d6576aee1dad
Hüseyin YİĞİT <yigit.hsyn@outlook.com>
2018-05-08 11:03:44 +0300

    Routing updated to api

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 var express = require('express')
 var bodyParser = require("body-parser")
 var cors = require("cors")
-var notifier = require('node-notifier');
 
 const PATH = require('path');
 const dirTree = require('directory-tree');
-
-//const tree = dirTree('./node_modules', {}, (item, PATH) => {
-//	console.log(item);
-//});
 
 const app = express();
 app.use(bodyParser.json())
@@ -16,15 +11,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.static(__dirname + "/test"))
 app.listen(3001, function () {
-  notifier.notify({
-    title: 'Service: 3001-dirtree',
-    message: "Started succesfully!",
-    wait: true
-  });
   console.log("Service running on http://127.0.0.1:3001")
 })
 
-app.get("/dirtree", function (req, res) {
+app.get("/api", function (req, res) {
   res.send(dirTree(req.query.path))
 })
-
