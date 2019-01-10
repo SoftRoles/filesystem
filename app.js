b9417f6f2c29fd81f366013437db1eef8d458e66
Hüseyin <yigit.hsyn@gmail.com>
2019-01-10 22:20:46 +0300

    api simplified

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ var express = require('express');
 var app = express();
 
 //=====================================
-// authorization check
-//=====================================
-const ensureLogin = require("@softroles/ensure-login").ensureLogin
-
-//=====================================
 // database
 //=====================================
 var assert = require('assert');
@@ -22,35 +17,21 @@ mongoClient.connect(mongodbUrl, { poolSize: 10, useNewUrlParser: true }, functio
 //=====================================
 // common middlewares
 //=====================================
-app.use(function (req, res, next) {
-  if (!req.user && req.ip.indexOf("127.0.0.1") > -1){
-    req.user = {username = "local"}
-  }
-  next()
-})
-
+app.use(require('@softroles/authorize-local-user')())
 app.use(require('morgan')('tiny'));
 app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require("cors")())
 
-app.get('/filesystem', ensureLogin({ redirectTo: "/login?source=filesystem" }), function (req, res) {
-  if (req.user.username == "admin") res.sendFile(__dirname + '/public/index.html')
-  else { req.logout(); res.send(403); }
-});
 
-app.get('/filesystem/test/upload', ensureLogin({ redirectTo: "/login?source=filesystem/test/upload" }), function (req, res) {
-  if (req.user.username == "admin") res.sendFile(__dirname + '/public/test/upload.html')
-  else { req.logout(); res.send(403); }
-});
 
-//==================================================================================================
+//=============================================================================
 // api
-//==================================================================================================
+//=============================================================================
 
-//-----------------------------------------------------------------------------
-// filesytem : files
-//-----------------------------------------------------------------------------
+//-------------------------------------
+// files
+//-------------------------------------
 var mkdirp = require("mkdirp")
 var fs = require("fs")
 var os = require('os');
@@ -59,11 +40,11 @@ var filesize = require("filesize")
 var moment = require("moment")
 var mongoObjectId = require('mongodb').ObjectID;
 
-var filesFolder = path.join(os.homedir(), "datas/1001-files")
+var filesFolder = path.normalize(path.join(__dirname,"../../Datas/files"))
 
 app.use(require('express-fileupload')())
 
-app.get('/filesystem/api/files', ensureLogin(), function (req, res) {
+app.get('/filesystem/api/files', function (req, res) {
   req.query.users = req.user.username
   mongodb.db("filesystem").collection("files").find(req.query).toArray(function (err, docs) {
     if (err) res.send({ error: err })
@@ -71,7 +52,7 @@ app.get('/filesystem/api/files', ensureLogin(), function (req, res) {
   });
 });
 
-app.post('/filesystem/api/files', ensureLogin(), function (req, res) {
+app.post('/filesystem/api/files', function (req, res) {
   if (!req.files) return res.sendStatus(400)
   mkdirp(path.join(filesFolder, req.body.folder), function (err) {
     if (err) res.send(err)
@@ -103,7 +84,7 @@ app.post('/filesystem/api/files', ensureLogin(), function (req, res) {
   })
 });
 
-app.get('/filesystem/api/files/:id', ensureLogin(), function (req, res) {
+app.get('/filesystem/api/files/:id', function (req, res) {
   var query = { users: req.user.username }
   query._id = mongoObjectId(req.params.id)
   mongodb.db("filesystem").collection("files").findOne(query, function (err, doc) {
@@ -114,38 +95,15 @@ app.get('/filesystem/api/files/:id', ensureLogin(), function (req, res) {
     }
   });
 });
-//-----------------------------------------------------------------------------
-// filesytem : operations
-//-----------------------------------------------------------------------------
-var fs = require("fs")
-var path = require("path")
-app.get("/filesystem/api/mkdir", ensureLogin(), function (req, res) {
-  if (req.user.username == "admin") {
-    fs.mkdir(path.join(filesFolder, req.query.path), function (err) {
-      if (err) res.send({ error: err })
-      else res.send({})
-    })
-  }
-})
 
-//-----------------------------------------------------------------------------
-// filesytem : dirtree
-//-----------------------------------------------------------------------------
+//-------------------------------------
+// directory tree
+//-------------------------------------
 const dirTree = require('directory-tree');
-app.get("/filesystem/api/dirtree", ensureLogin(), function (req, res) {
+app.get("/filesystem/api/dirtree", function (req, res) {
   if (req.user.username == "admin") res.send(dirTree(filesFolder))
   else res.send(403);
 })
-
-//-----------------------------------------------------------------------------
-// filesytem : homedir
-//-----------------------------------------------------------------------------
-var os = require("os")
-app.get("/filesystem/api/homedir", ensureLogin(), function (req, res) {
-  if (req.user.username == "admin") res.send(os.homedir() + "\\desktop")
-  else { req.logout(); res.send(403); }
-})
-
 
 app.listen(3001, function () {
   console.log("Service running on http://127.0.0.1:3001")
